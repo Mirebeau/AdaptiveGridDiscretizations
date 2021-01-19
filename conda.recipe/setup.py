e8cede7fb596b5b3588823d87ca29ae852bff66c
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2021-01-19 21:17:39 +0100

    Update TOcs

--- conda.recipe/setup.py
+++ conda.recipe/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name='agd',  
-	version='0.1.21',
+	version='0.1.22',
 	author="Jean-Marie Mirebeau",
 	author_email="jm.mirebeau@gmail.com",
 	description="Adaptive Grid Discretizations",
