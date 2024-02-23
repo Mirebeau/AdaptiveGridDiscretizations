fceca560ae516ff6dcf63b0c22f836cc6df51fc4
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2024-02-23 23:35:05 +0100

    Periodic interpolation along some axes

--- agd/Interpolation.py
+++ agd/Interpolation.py
@@ -114,7 +114,7 @@ def spline_base(x,order=3):
 	x5 = x*x4
 	if order==5: return (x5,1+5*x+10*x2+10*x3+5*x4-5*x5,26+50*x+20*x2-20*x3-20*x4+10*x5,66-60*x2+30*x4-10*x5,26-50*x+20*x2+20*x3-20*x4+5*x5,1-5*x+10*x2-10*x3+5*x4-x5)
 
-def spline_weighted(c,x,order=3,overwrite_x=False):
+def spline_weighted(c,x,order=3,overwrite_x=False,periodic=False):
 	"""
 	Evaluate splines at position x, weighted by coefficients coef, with reflected boundary conditions
 	- c : spline coefficients, array of shape (m1,...,mk,n1,...,nd)
@@ -135,9 +135,10 @@ def spline_weighted(c,x,order=3,overwrite_x=False):
 	x_i = np.floor(x).astype(int_t)
 	x -= x_i # 0<=x<=1, so that we can evaluate the spline 
 	xp = ad.cupy_generic.get_array_module(x)
+	x_i = x_i[:,None] + xp.arange(1+order,dtype=int_t)[:,None] # shape = (d,1+order,p)
 	dom_shape_arr = xp.array(dom_shape,dtype=int_t)[:,None,None]
-	x_i = x_i[:,None] + xp.arange(1+order,dtype=int_t)[:,None] # shape (d,1+order,*p)
-	x_i = x_i % (2*dom_shape_arr)
+	periodic = np.expand_dims(xp.array(periodic),axis=(-2,-1))
+	x_i = x_i % np.where(periodic,dom_shape_arr,2*dom_shape_arr)
 	refl = x_i>=dom_shape_arr
 	x_i[refl] = (2*dom_shape_arr-1-x_i)[refl]
 	x_i = tuple(z.reshape( (1,)*k + (order+1,) + (1,)*(x_dim-k-1)+(-1,)) for k,z in enumerate(x_i))
@@ -147,7 +148,7 @@ def spline_weighted(c,x,order=3,overwrite_x=False):
 		c = np.sum(c*spline_val,axis=-2)
 	return c.reshape(c_shape+x_shape)
 
-def spline_coefs(c,order=3,depth=0):
+def spline_coefs(c,order=3,depth=0,periodic=False):
 	"""
 	Input : 
 	- c : values of the function to be interpolated
@@ -158,23 +159,26 @@ def spline_coefs(c,order=3,depth=0):
 	"""
 	if order==1: return c # Bypass for first order splines (hat function)
 	assert order%2==1
+	ord2 = order//2
 	xp = ad.cupy_generic.get_array_module(c)
 	solver = scipy.linalg.solve_circulant
 	dom_shape = c.shape[depth:]
 	spline_vals = xp.asarray(spline_base(0,order))
-	for i,s in enumerate(dom_shape):
-		circ = np.zeros_like(ad.remove_ad(c),shape=(2*s,))
-		circ[0] = spline_vals[-1]; circ[-order:] = spline_vals[:-1]
+	if not isinstance(periodic,tuple): periodic=(periodic,)*len(dom_shape)
+	for i,(s,per) in enumerate(zip(dom_shape,periodic)):
 		c = np.moveaxis(c,depth+i,0)
-		c = np.concatenate((c,np.flip(c,axis=0)))
+		if per: circ = np.zeros_like(ad.remove_ad(c),shape=(s,))
+		else: circ = np.zeros_like(ad.remove_ad(c),shape=(2*s,)); c = np.concatenate((c,np.flip(c,axis=0)))
+		circ[0] = spline_vals[-1]; circ[-order:] = spline_vals[:-1]
 		if isinstance(c,ad.Dense.denseAD):c=ad.Dense.denseAD(solver(circ,c.value), solver(circ,c.coef))
 		elif isinstance(c,ad.Dense2.denseAD2):c=ad.Dense2.denseAD2(solver(circ,c.value), solver(circ,c.coef), solver(circ,c.coef2))
 		else: c = solver(circ,c)
-		c = np.moveaxis(c[order//2:order//2+len(c)//2],0,depth+i)
+		c = np.roll(c,-ord2) if per else c[ord2:ord2+s]
+		c = np.moveaxis(c,0,depth+i)
 	return c
 
 def map_coordinates(input,coordinates,output=None,order=3,mode='constant',cval=0.0,prefilter=True,
-	grid=None):
+	grid=None,periodic=False):
 	"""
 	Partial reimplementation of map_coordinates, which allows (input and) coordinates to be AD arrays.
 	Appears to be much more accurate that ndimage.map_coordinates, which likely uses float32 internally
@@ -182,11 +186,12 @@ def map_coordinates(input,coordinates,output=None,order=3,mode='constant',cval=0
 	- grid : rescale the coordinates according to this grid
 	"""
 	assert output is None
-	assert mode=='reflect'
+	assert mode in ('reflect','grid-mirror','grid-wrap')
+	if mode=='grid-wrap': periodic=True
 	coordinates = coordinates_on_grid(coordinates,grid)
-	if prefilter: coefs = spline_coefs(input,order,depth=input.ndim-len(coordinates))
+	if prefilter: coefs = spline_coefs(input,order,depth=input.ndim-len(coordinates),periodic=periodic)
 	else: coefs = input/math.factorial(order)
-	return spline_weighted(coefs,coordinates,order,overwrite_x=grid is not None)
+	return spline_weighted(coefs,coordinates,order,periodic=periodic,overwrite_x=grid is not None)
 
 class UniformGridInterpolation:
 	"""
@@ -222,437 +227,3 @@ class UniformGridInterpolation:
 	def __call__(self,x):
 		x = coordinates_on_grid(x,origin=self.origin,scale=self.scale)
 		return spline_weighted(self.coefs,x,self.order,overwrite_x=True)
-
-
-# # ------------------------------------------------------------------------------
-# # Below : incorrect boundary conditions. Deprecated.
-
-# class _spline_univariate:
-# 	"""
-# 	A univariate spline of a given order, with not-a-knot boundary conditions.
-# 	"""
-# 	def __init__(self,order,shape,periodic):
-# 		assert isinstance(order,int)
-# 		self.order=order
-# 		if not (order>=1 and order<=3):
-# 			raise ValueError("Unsupported spline order")
-
-# 		assert isinstance(shape,int) and shape>=0
-# 		self.shape = shape
-
-# 		assert isinstance(periodic,bool)
-# 		self.periodic=periodic
-
-
-# 	def __call__(self,xa,xs):
-# 		"""
-# 		Weight at absolute position xa, of of spline centered at xs.
-# 		"""
-# 		xa=ad.asarray(xa)
-# 		xs=ad.asarray(xs)
-# 		if   self.order==1: return self._call1(xa,xs)
-# 		elif self.order==2: return self._call2(xa,xs)
-# 		elif self.order==3: return self._call3(xa,xs)
-# 		else: assert False
-
-# 	def nodes(self,interior):
-# 		"""
-# 		Returns range(a,b) where [x+a,x+b] contains the support
-# 		 of the spline centered at some point x (interior or boundary) 
-# 		"""
-# 		if   self.order==1: return range(-1,1)
-# 		elif self.order==2: #return range(-1,2)
-# 			return range(-1,2) if interior else range(-3,3)
-# 		elif self.order==3: 
-# 			return range(-2,2) if interior else range(-3,4)
-# 		assert False
-
-# 	def interior(self,x,tol=None):
-# 		"""
-# 		Wether the interior nodes can be used, or one should fall back to boundary nodes.
-# 		"""
-
-# 		# We tolerate a bit of extrapolation		
-# 		if np.any(x<=-1) or np.any(x>=self.shape):
-# 			raise ValueError("Interpolating data outside domain")
-# 		if self.order==1 or self.periodic:
-# 			return np.full_like(x,True,dtype='bool')
-# 		elif self.order==2:
-# 			return x>=1
-# 		elif self.order==3:
-# 			return np.logical_and(x>=1,x<=self.shape-2)
-
-# 	def _call1(self,xa,xs):
-# 		"""
-# 		A piecewise linear spline, defined over [-1,1].
-# 		"""
-# 		x=xa-xs.astype(xa.dtype) # Avoid float32 + int32 -> float64 cast on GPU
-# 		result = np.zeros_like(x)
-# 		seg=ad.asarray(np.floor(x+1))
-		
-# 		# relative interval [-1,0[
-# 		pos = seg==0
-# 		result[pos] = 1+x[pos]
-
-# 		# relative interval [0,1[
-# 		pos = seg==1
-# 		result[pos] = 1-x[pos]
-
-# 		return result
-
-# 		# Rejected because derivative at node points is inconsistent accross nodes
-# 		# return np.maximum(0.,1.-np.abs(x))
-
-# 	def _call2(self,xa,xs):
-# 		"""
-# 		A piecewise quadratic spline function, defined over [-1,2]
-# 		"""
-# 		x=ad.asarray(xa-xs.astype(xa.dtype))
-# 		result = np.zeros_like(x)
-
-# 		# Which spline segment to use
-# 		seg = ad.asarray(np.floor(x+1))
-
-# 		if not self.periodic:
-# 			# not-a-knot boundary condition : obtain a pieceiwise quadratic polynomial f which
-# 			# - interpolates the values at all nodes
-# 			# - has f' continuous at all interior nodes
-# 			# - has f'' = 0 at the left boundary node
-
-# 			# I do not know what this code does but it is clearly buggy does not implement the condition
-# 			pos = np.logical_and(xa<=1,xs<=2)
-# 			seg[pos] = 2-xs[pos]
-# 			s=self.shape-1
-# 			pos = np.logical_and(xa>=s-1,xs>=s-2)
-# 			seg[pos] = s-xs[pos]
-
-# 		# relative interval [-1,0[
-# 		pos = seg==0
-# 		x_ = x[pos]+1
-# 		result[pos] = x_**2
-
-# 		# relative interval [0,1[
-# 		pos = seg==1
-# 		x_ = x[pos]-0.5
-# 		result[pos] = 1.5 - 2.*x_**2
-
-# 		# relative interval [1,2[
-# 		pos = seg==2
-# 		x_ = 2.-x[pos]
-# 		result[pos] = x_**2 
-
-# 		return result
-
-# 	def _call3(self,xa,xs):
-# 		"""
-# 		A piecewise cubic spline function, defined over [-2,2]
-# 		"""
-# 		x=ad.asarray(xa-xs)
-# 		result = np.zeros_like(x)
-
-# 		# Which spline segment to use
-# 		seg = ad.asarray(np.floor(x+2))
-# 		if not self.periodic:
-# 			# not-a-knot boundary condition : obtain a pieceiwise cubic polynomial f which
-# 			# - interpolates the values at all nodes
-# 			# - has f' and f'' continuous at all interior nodes
-# 			# - has f'' = 0 at boundary nodes
-
-# 			# I do not know what this code does but it is clearly buggy does not implement the condition
-# 			pos = np.logical_and(xa<=1,xs<=3)
-# 			seg[pos] = 3-xs[pos]
-# 			s=self.shape-1
-# 			pos = np.logical_and(xa>=s-1,xs>=s-3)
-# 			seg[pos] = s-xs[pos]
-
-# 		def f0(y): return y**3
-# 		def f1(y): return 1+3*y+3*y**2-3.*y**3
-
-# 		# relative interval [-2,-1[
-# 		pos = seg==0
-# 		result[pos] = f0(x[pos]+2.)
-
-# 		# relative interval [-1,0[
-# 		pos = seg==1
-# 		result[pos] = f1(x[pos]+1.)
-
-# 		# relative interval [0,1[
-# 		pos = seg==2
-# 		result[pos] = f1(1.-x[pos])
-
-# 		# relative interval [1,2[
-# 		pos = seg==3
-# 		result[pos] = f0(2.-x[pos])
-		
-# 		"""
-# 		if not self.periodic:
-# 			# End of not-a-knot boundary condition
-# 			def g(y): return 4.-6.*y**2+(50./27.)*y**3
-# 			xa=ad.broadcast_to(xa,x.shape)
-# 			pos = np.logical_and(xa<=3,xs==3)
-# 			result[pos] = g(3.-xa[pos])
-# 			pos = np.logical_and(xa>s-3,xs==s-3)
-# 			result[pos] = g(xa[pos]-(s-3))
-# 		"""
-
-# 		return result
-
-# 	def _band(self):
-# 		rg = np.arange(self.shape+0.)
-# 		if self.order==2:
-# 			band_tr = np.stack((self(rg,rg-1),self(rg,rg),self(rg,rg+1),self(rg,rg+2)),axis=0)
-# 			return _banded_transpose((2,1),band_tr)
-# 		elif self.order==3:
-# 			band_tr = np.stack((self(rg,rg-3),self(rg,rg-2),self(rg,rg-1),
-# 				self(rg,rg),self(rg,rg+1),self(rg,rg+2),self(rg,rg+3)),axis=0)
-# 			return _banded_transpose((3,3),band_tr)
-# 		else: assert False
-
-
-# 	def make_coefs(self,values,overwrite_values=False):
-# 		"""
-# 		Produces the node coefficients corresponding to given values.
-# 		!! Call convention : interpolation is along the first axis. !!
-# 		"""
-# 		if self.order==1: return values
-# 		if self.periodic: raise ValueError("Periodic interpolation is not supported for degree > 1")
-# 		assert len(values)==self.shape
-
-# 		def _solver(vals): 
-# 			return scipy.linalg.solve_banded(*self._band(),vals,
-# 				overwrite_ab=True,overwrite_b=overwrite_values)
-
-# 		def solver(vals): 
-# 			if ad.cupy_generic.from_cupy(vals):
-# 				# Cupy 13.0 does not implement a banded solver. Warn ? 
-# 				#print("Interpolation, performance warning : banded solve performed on CPU") 
-# 				import cupy as cp
-# 				return cp.asarray(_solver(vals.get()),dtype=vals.dtype)
-# 			return _solver(vals)
-
-# 		if ad.is_ad(values): return values.apply_linear_operator(solver)
-# 		return solver(values)
-		
-# def _banded_transpose(lu,t):
-# 	l,u=lu
-# 	return (u,l),np.stack(tuple(np.roll(t[i],i-u) for i in reversed(range(l+u+1))))
-# #	return np.stack((np.roll(t[2],1),t[1],np.roll(t[0],-1)),axis=0)
-
-# def _banded_densify(lu,t):
-# 	"""Turn banded matrix in to dense matrix (inefficient, for testing)"""
-# 	l,u=lu
-# 	n = t.shape[1]
-# 	mat = np.zeros((n,n))
-# 	for i in range(n):
-# 		for j in range(n):
-# 			k=u+i-j
-# 			if 0<=k and k<len(t):
-# 				mat[i,j]=t[k,j]
-# 	return mat
-
-# class _spline_tensor:
-# 	"""
-# 	A tensor product of univariate splines.
-# 	"""
-# 	def __init__(self,orders,shape,periodic):
-# 		assert all(isinstance(t,tuple) for t in (orders,shape,periodic))
-# 		self.splines = tuple(_spline_univariate(order,s,per) 
-# 			for (order,s,per) in zip(orders,shape,periodic))
-# 		assert all(len(t)==self.vdim for t in (orders,shape,periodic))
-
-# 	@property
-# 	def order(self):
-# 		return tuple(spline.order for spline in self.splines)
-# 	@property
-# 	def shape(self):
-# 		return tuple(spline.shape for spline in self.splines)
-# 	@property
-# 	def periodic(self):
-# 		return tuple(spline.periodic for spline in self.splines)
-	
-# 	@property
-# 	def vdim(self):
-# 		return len(self.splines)
-
-# 	def __call__(self,xa,xs):
-# 		"""
-# 		Weight at absolute position xa, of of spline centered at xs.
-# 		"""
-# 		return np.prod( ad.asarray(tuple(spline(xai,xsi) 
-# 			for (xai,xsi,spline) in zip(xa,xs,self.splines))), axis=0)
-
-# 	def nodes(self,interior):
-# 		"""
-# 		The spline at x (interior or boundary) is supported 
-# 		on the union of x+node+[0,1]**vdim
-# 		"""
-# 		_nodes = tuple(spline.nodes(interior) for spline in self.splines)
-# 		return np.asarray(tuple(itertools.product(*_nodes))).T
-
-# 	def interior(self,x):
-# 		"""
-# 		Wether the interior nodes can be used.
-# 		"""
-# 		return ad.asarray(tuple(spline.interior(xi) 
-# 			for (spline,xi) in zip(self.splines,x))).all(axis=0)
-# #		return np.logical_and.reduce(tuple(spline.interior(xi) # cupy has no reduce
-# #			for (spline,xi) in zip(self.splines,x)))
-
-# 	def make_coefs(self,values,overwrite_values=False):
-# 		"""
-# 		Produces the node coefficients corresponding to given values.
-# 		!! Call convention : interpolation is along the first axes. 
-# 		"""
-# 		for i,spline in enumerate(self.splines): #reversed(list(enumerate(self.splines))):
-# 			values = np.moveaxis(spline.make_coefs(
-# 				np.moveaxis(values,i,0),overwrite_values=overwrite_values),0,i)
-# 		return values
-
-# class UniformGridInterpolation:
-# 	"""
-# 	Interpolates values on a uniform grid, in arbitrary dimension, using splines of 
-# 	a given order.
-# 	"""
-
-# 	def __init__(self,grid,values=None,order=1,periodic=False,check_grid=True):
-# 		"""
-# 		- grid (ndarray) : must be a uniform grid. E.g. np.meshgrid(aX,aY,indexing='ij')
-# 		 where aX,aY have uniform spacing. Alternatively, provide only the axes.
-# 		- values (ndarray) : interpolated values.
-# 		- order (int, tuple of ints) : spline interpolation order (<=3), along each axis.
-# 		- periodic (bool, tuple of bool) : wether periodic interpolation, along each axis.
-# 		"""
-# #		print("agd.Interpolation.UniformGridInterpolation is deprecated")
-
-# 		if isinstance(grid,dict):
-# 			self.shape  = grid['shape']
-# 			self.origin = ad.asarray(grid['origin'])
-# 			self.scale  = ad.asarray(grid['scale'])
-# 			if grid.get('cell_centered',False): 
-# 				self.origin += self.scale/2 # Convert to node_centered origin
-# 		else:
-# 			self.origin,self.scale,self.shape = origin_scale_shape(grid)
-# 			if check_grid and grid[0].ndim>1:
-# 				assert np.allclose(grid,self._grid(),atol=1e-5) #Atol allows float32 types
-
-# 		if order is None: order = 1
-# 		if isinstance(order,int): order = (order,)*self.vdim
-
-# 		if periodic is None: periodic=False
-# 		if isinstance(periodic,bool): periodic = (periodic,)*self.vdim
-# 		self.periodic=periodic
-
-# 		self.spline = _spline_tensor(order,self.shape,periodic)
-# 		assert self.spline.vdim == self.vdim
-# 		self.interior_nodes = self.spline.nodes(interior=True)
-# 		self.boundary_nodes = self.spline.nodes(interior=False)
-
-# 		self.coef = None if values is None else self.make_coefs(values)
-
-# 	@property
-# 	def vdim(self):
-# 		"""
-# 		Vector dimension of the interpolation points.
-# 		"""
-# 		return len(self.shape)
-# 	@property
-# 	def oshape(self):
-# 		"""
-# 		Number of dimensions of the interpolated objects.
-# 		"""
-# 		return self.coef.shape[self.vdim:]
-
-# 	def __call__(self,x,interior=None):
-# 		"""
-# 		Interpolates the data at the position x.
-# 		- interior : set to true if all values are inside the interpolation domain,
-# 		false if some are outside (extrapolation), or None for autodetect
-# 		"""
-# 		x=ad.asarray(x)
-# 		assert len(x)==self.vdim
-		
-# 		pdim = x.ndim-1 # Number of dimensions of position
-# 		origin,scale = (_append_dims(e,pdim) for e in (self.origin,self.scale))
-
-# 		# Separate treatment of interior and boundary points
-# 		if interior is None:
-# 			y = (ad.remove_ad(x) - origin)/scale
-# 			interior_x = self.spline.interior(y)
-# 			boundary_x = np.logical_not(interior_x)
-# 			interior_result = self(x[:,interior_x],True)
-# 			boundary_result = self(x[:,boundary_x],False)
-
-# 			result_shape = self.oshape+x.shape[1:]
-# 			if result_shape==tuple(): #numpy zeros_like has a bug for empty shapes
-# 				some_result = interior_result if interior_result.size>0 else boundary_result
-# 				result = np.zeros_like(some_result.reshape(-1)[0])
-# 			else: result = np.zeros_like(interior_result,shape=result_shape)
-
-# 			try:
-# 				result[...,interior_x] = interior_result
-# 				result[...,boundary_x] = boundary_result
-# 			except (ValueError,IndexError):
-# 				# Some cupy versions do not handle Ellipsis correctly
-# 				ellipsis = (slice(None),)*len(self.oshape)
-# 				result.__setitem__((*ellipsis,interior_x),interior_result)
-# 				result.__setitem__((*ellipsis,boundary_x),boundary_result)
-
-# 			return result
-
-# 		# Rescale the coordinates in reference rectangle
-# 		y = np.expand_dims((x - origin)/scale,axis=1)
-# 		# Bottom left pixel
-# 		yf = np.floor(y).astype(int)
-# 		# All pixels supporting an active spline
-# 		nodes = self.interior_nodes if interior else self.boundary_nodes
-# 		ys = yf - _append_dims(nodes,pdim)
-		
-# 		# Spline coefficients, taking care of out of domain 
-# 		ys_ = ys.copy()
-# 		out = np.full_like(ad.remove_ad(x[0]),False,dtype=bool)
-# 		for i,(d,per) in enumerate(zip(self.shape,self.periodic)):
-# 			if per: 
-# 				ys_[i] = ys_[i]%d
-# 			else: 
-# 				bad = np.logical_or(ys_[i]<0,ys_[i]>=d)
-# 				out = np.logical_or(out,bad)
-# 				try: 
-# 					ys_[i,bad] = 0 
-# 				except ValueError: # Old cupy versions do not support such slices
-# 					ys_i = ys_[i]
-# 					ys_i[bad] = 0
-# 					ys_[i] = ys_i
-
-# 		coef = self.coef[tuple(ys_)]
-# 		coef[out]=0.
-# 		ondim = len(self.oshape)
-# 		coef = np.moveaxis(coef,range(-ondim,0),range(ondim))
-
-# 		# Spline weights
-# 		weight = self.spline(y,ys)
-# 		return (coef*weight).sum(axis=ondim)
-
-# 	def set_values(self,values):
-# 		self.coef = self.make_coefs(values)
-
-# 	def make_coefs(self,values,overwrite_values=False):
-# 		values = ad.asarray(values)
-# 		xp = ad.cupy_generic.get_array_module(values)
-# 		self.interior_nodes = xp.array(self.interior_nodes)
-# 		self.boundary_nodes = xp.array(self.boundary_nodes)
-
-# 		ondim = values.ndim - self.vdim
-# 		# Internally, interpolation is along the first axes.
-# 		# (Contrary to external interface)
-# 		val = np.moveaxis(values,range(ondim),range(-ondim,0))
-
-# 		return self.spline.make_coefs(val,overwrite_values=overwrite_values)
-
-# 	def _grid(self):
-# 		xp = ad.cupy_generic.get_array_module(self.origin)
-# 		dtype = self.origin.dtype
-# 		return ad.asarray(np.meshgrid(*(o+h*xp.arange(s+0.,dtype=dtype) 
-# 			for (o,h,s) in zip(self.origin,self.scale,self.shape)), indexing='ij'))
-
-
-
