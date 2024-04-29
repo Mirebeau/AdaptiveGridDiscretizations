a4ee68bbafadb1b256be5e890352cec500210c88
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2024-04-29 12:56:26 +0200

    Passing tests

--- agd/ExportedCode/Notebooks_Div/ElasticEnergy.py
+++ agd/ExportedCode/Notebooks_Div/ElasticEnergy.py
@@ -11,3 +11,26 @@ norm_infinity = ad.Optimization.norm_infinity
 norm_average = ad.Optimization.norm_average
 mica,_ = Hooke.mica # Hooke tensor associated to this crystal
 
+import numpy as np; xp=np; allclose=np.allclose
+import matplotlib.pyplot as plt
+from copy import copy
+import scipy.sparse; import scipy.sparse.linalg
+import itertools
+
+def ElasticEnergy(v,hooke,dom):
+    """
+    Finite differences approximation of c(ϵ,ϵ), where c is the Hooke tensor and ϵ the strain tensor,
+    which is twice the (linearized) elastic energy density.
+    """
+    d=len(v)
+    coefs,moffsets = hooke.Selling()
+    dvp = tuple( dom.DiffUpwind(v[i], moffsets[i]) for i in range(d))
+    dvm = tuple(-dom.DiffUpwind(v[i],-moffsets[i]) for i in range(d))
+    
+    # Consistent approximations of Tr(moffset*grad(v))
+    dv = ad.array([sum(s) for s in itertools.product(*zip(dvp,dvm))])
+    dv2 = (dv**2).sum(axis=0)
+    
+    coefs = fd.as_field(coefs,v.shape[1:]) * 2**(-d)
+    return (coefs*dv2).sum(axis=0) 
+
